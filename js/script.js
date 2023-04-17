c0dd041ccdab1dd85f9ac6c8e9c16355a0fb117e
KarmAWalruS <111267491+KarmAWalruS@users.noreply.github.com>
2023-04-17 08:26:43 +0300

    Add files via upload
    final version with animation

--- js/script.js
+++ js/script.js
@@ -81,3 +81,17 @@ function togglePopupInterior() {
     cards.gallery__interior.classList.add("overlay__hidden");
   }
 }
+
+function onEntry(entry) {
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add("element-show");
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".element-animation");
+for (let elm of elements) {
+  observer.observe(elm);
+}
