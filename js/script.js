9650352747ed6cdbad9ca0c7dea5f13722ea822b
KarmAWalruS <111267491+KarmAWalruS@users.noreply.github.com>
2023-08-10 08:10:57 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -89,7 +89,7 @@ function onEntry(entry) {
     }
   });
 }
-let options = { threshold: [0.5] };
+let options = { threshold: [0.3] };
 let observer = new IntersectionObserver(onEntry, options);
 let elements = document.querySelectorAll(".element-animation");
 for (let elm of elements) {
